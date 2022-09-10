0ab127f6b56fe240a59272dfac3979c5cbcecdda
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-10 08:31:15 +0530

    DataTypes

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,31 @@
-let js = "amazing";
-if (js === "amazing") alert("Js is amazing");
+// let js = "amazing";
+// if (js === "amazing") alert("Js is amazing");
 
 
 console.log(40 / 52 * 45 + 47 - 10 + 50);
+console.log(40);
+console.log("Newaskar");
+
+let firstName = "Pranali";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//88888888//  Assignment No 1  //88888888888//
+
+// let country = "India";
+// let continent = "Asia";
+// let population = "1.35 billion";
+
+// console.log(country, continent, population)
+
+//8888888888888888888888888888888888888888888//
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof firstName);
+console.log(typeof 23);
+console.log(typeof "Rupam");

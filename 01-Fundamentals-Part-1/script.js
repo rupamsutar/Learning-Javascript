21ba9360eebe22494b63b4f8be1f0e0d4b92b71a
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-11 08:00:24 +0530

    Basic Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,14 +2,14 @@
 // if (js === "amazing") alert("Js is amazing");
 
 
-console.log(40 / 52 * 45 + 47 - 10 + 50);
-console.log(40);
-console.log("Newaskar");
+// console.log(40 / 52 * 45 + 47 - 10 + 50);
+// console.log(40);
+// console.log("Newaskar");
 
-let firstName = "Pranali";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Pranali";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 //88888888//  Assignment No 1  //88888888888//
 
@@ -21,11 +21,20 @@ console.log(firstName);
 
 //8888888888888888888888888888888888888888888//
 
-let javascriptIsFun = true;
+// let javascriptIsFun = true;
 
-console.log(javascriptIsFun);
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof firstName);
-console.log(typeof 23);
-console.log(typeof "Rupam");
+// console.log(typeof true);
+// console.log(typeof firstName);
+// console.log(typeof 23);
+// console.log(typeof "Rupam");
+
+
+const now = 2022;
+const agePranali = 2022 - 1999;
+const ageRupam = 2022 - 1998;
+
+console.log(agePranali, ageRupam);
+
+console.log(2 ** 5, ageRupam * 2, agePranali + 10);

472811dd743fc6becc079a098b585ca3ef61106a
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-12 09:11:45 +0530

    Significance of using Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicense = false;
+let passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can Drive !");
+
+// why using strict is signified in the example above.
+

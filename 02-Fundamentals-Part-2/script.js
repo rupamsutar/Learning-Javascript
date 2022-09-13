298a3e7f08926ff64d5d3c743c5b41ef984f941a
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-13 11:25:30 +0530

    Arrow Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -36,6 +36,8 @@
 
 //Function Declaration
 
+/*
+
 function calcAge1(birthYear) {
     return (2022 - birthYear);
 };
@@ -50,11 +52,19 @@ const calcAge2 = function (birthYear) {
 
 console.log(calcAge2(1999));
 
-/*
+
 
 Function can be called before the "Function Declarations are formed".
-However,
-In the function Expressions we cannot call the function before.
-
 */
 
+// Arrow Functions !
+
+const calcAge3 = birthYear => 2022 - birthYear;
+
+console.log(calcAge3(1996));
+
+const yearsUntillRetirement = (birthYear, firstName) => {
+    return (`${firstName} has left ${58 - (2022 - birthYear)} years of Service !`);
+};
+
+console.log(yearsUntillRetirement(1996, "Revan"));

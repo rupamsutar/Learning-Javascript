95e7460f1b0376c34aec6f314b405b72fd351b10
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-17 18:56:07 +0530

    This keyword

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -1,1 +1,25 @@
 'use strict';
+ 
+console.log(this);
+
+function calcAge(birthYear) {
+    console.log(2022-birthYear);
+    console.log(this);
+};
+
+calcAge(1998);
+
+const calcAgePranali = birthYear => {
+    console.log(2022-birthYear);
+    console.log(this);
+} 
+calcAgePranali(1999);
+
+const Rupam = {
+    birthYear: 1998,
+    profession: "WebDeveloper",
+    calcAge: function() {
+        console.log(this);
+    }
+};
+Rupam.calcAge();

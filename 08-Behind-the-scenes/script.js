39a055be0c13c780d67379cda8235d8fead13a0c
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-18 12:36:06 +0530

    xyz

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
  
+/*
 console.log(this);
 
 function calcAge(birthYear) {
@@ -23,3 +24,20 @@ const Rupam = {
     }
 };
 Rupam.calcAge();
+
+*/
+
+const Rupam = {
+    firstName: "Rupam",
+    birthYear: 1998,
+    profession: "WebDeveloper",
+    calcAge: function() {
+        console.log(this);
+    },
+
+    
+
+    
+};
+
+

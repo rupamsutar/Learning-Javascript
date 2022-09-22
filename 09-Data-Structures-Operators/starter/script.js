c58a2470c2e45f13e9862bbf4d4f0a8fd13d4015
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-22 15:39:35 +0530

    Enhanced object Literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -12,34 +28,21 @@ const Restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function(starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  openingHours,
 
-  orderDelivery: function({starterIndex, mainIndex, time, address}) {
+  orderDelivery({starterIndex, mainIndex, time, address}) {
     console.log(`Order Recieved of ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${time} on the address : ${address}`);
   },
 
-  orderMaggie: function(ing1, ing2, ing3) {
+  orderMaggie(ing1, ing2, ing3) {
     console.log(`Here is your maggie with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderBurger: function(mainIngredient, ...otherIngredients) {
+  orderBurger(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(...otherIngredients);
   },
@@ -49,34 +52,51 @@ const Restaurant = {
   
 };
 
+//Enhanced Object Literals:
 
+
+
+console.log(Restaurant)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 //The for-of Loop
 
 const menu = [...Restaurant.starterMenu, ...Restaurant.mainMenu];
 
 for (const [i, el] of menu.entries()) console.log(`${i + 1} : ${el}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //challenge 1

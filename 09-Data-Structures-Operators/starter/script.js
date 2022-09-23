6a84c18f1eaba7ae543f55862b34313510983a50
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-23 19:21:11 +0530

    Challenge  3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,59 @@ const Restaurant = {
   
 };
 
+
+//Coding Challenge 3:
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+  ]);
+
+
+const setEvents = [...new Set([...gameEvents.values()])];
+console.log(setEvents);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+for (const [key, value] of gameEvents){
+  const half = key <= 45 ? "[FIRST HALF]" : "[SECOND HALF]";
+  console.log(`${half} ${key}: ${value}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 //Maps :
 
 const orderSet = new Set(["Pizza", "Maggie", "Coffee", "Maggie", "Pizza", "Dosa"]);
@@ -139,22 +192,6 @@ console.log([...question.values()]);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 
 
 //Challenge 2 :

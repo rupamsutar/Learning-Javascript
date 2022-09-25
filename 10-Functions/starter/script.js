f0c797d87146dfc839a673899fa7735c461462b7
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-25 17:04:56 +0530

    Value vs Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,62 @@
 'use strict';
 
+const flight = "A343V";
+
+const rupam = {
+    name: "Rupam Sutar",
+    passportNo: 25548569
+}
+
+const checkIn = function (flightNo, passenger) {
+    flightNo = "LH999";
+    passenger.name = "Mr. " + passenger.name;
+
+    if (passenger.passportNo === 25548569) {
+        alert("Checked In");
+    } else {
+        alert("Wrong Passport !")
+    }
+}
+
+checkIn(flight, rupam)
+
+console.log(flight);
+console.log(rupam);
+
+function newPassport(passenger) {
+    passenger.passportNo = Math.trunc(Math.random() * 10000000000)
+};
+
+newPassport(rupam);
+checkIn(flight, rupam);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 //Default Parameters: 
 
 const bookings = [];
@@ -24,3 +81,5 @@ createBooking(5);
 createBooking("A320", 4);
 createBooking("A330", 5, 2999);
 createBooking("B737", undefined, 4545);
+
+*/ 

6baa69391dd564a151cd2d3be089253b2ac50536
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-25 15:16:14 +0530

    Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//Default Parameters: 
+
+const bookings = [];
+ 
+const createBooking = function(flightNumber, numPassengers = 1, price = numPassengers * 1000) {
+
+    // numPassengers = numPassengers || 1;
+    // price = price || 1000;
+
+
+    const booking = {
+        flightNumber,
+        numPassengers,
+        price
+    }
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking(5);
+createBooking("A320", 4);
+createBooking("A330", 5, 2999);
+createBooking("B737", undefined, 4545);

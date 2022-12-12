503e3dc35a50fecc54236339c96f60e9d29a7ba3
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-12 10:10:36 +0530

    cleared for es6 syntax

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -3,29 +3,62 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ///////////////////////////////////////
- const request = new XMLHttpRequest();
+//  const request = new XMLHttpRequest();
 
- request.open("GET", "https://restcountries.com/v3.1/alpha/IN");
- request.send();
+//  request.open("GET", "https://restcountries.com/v3.1/alpha/IN");
+//  request.send();
 
-//  console.log(request.responseText);
+// //  console.log(request.responseText);
 
-request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-    <article class="country">
-        <img class="country__img" src="${data.flags.svg}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name.common}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} B people</p>
-          <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-          <p class="country__row"><span>💰</span>${data.currencies.INR.name}</p>
-        </div>
-    </article>`;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-});
+//     const html = `
+//     <article class="country">
+//         <img class="country__img" src="${data.flags.svg}" />
+//         <div class="country__data">
+//           <h3 class="country__name">${data.name.common}</h3>
+//           <h4 class="country__region">${data.region}</h4>
+//           <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} B people</p>
+//           <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+//           <p class="country__row"><span>💰</span>${data.currencies.INR.name}</p>
+//         </div>
+//     </article>`;
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+// });
